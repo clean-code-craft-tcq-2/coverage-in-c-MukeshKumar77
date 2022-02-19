a7a7c3a7e9a89e6197155b77d9f6f3904d91c87d
Mukesh <90908610+MukeshKumar77@users.noreply.github.com>
2022-02-19 17:59:51 +0530

    Create classify_and_check_breah.c

---
+++ classify_and_check_breah.c
@@ -0,0 +1,33 @@
+#include "check_and_alert.h"
+
+bool BatteryStatus;
+
+const double coolingTypeLimits[][2] = {{PASSIVE_COOLING_LOW_LIMIT, PASSIVE_COOLING_HI_LIMIT},
+										{HI_ACTIVE_COOLING_LOW_LIMIT, HI_ACTIVE_COOLING_HI_LIMIT},
+										{MED_ACTIVE_COOLING_LOW_LIMIT, MED_ACTIVE_COOLING_HI_LIMIT}};
+
+BreachType classifyTemperatureBreach(BatteryTypeAndInfo batteryInfo)
+{
+	 double lowerLimit = coolingTypeLimits[batteryInfo.coolingType][0];
+	 double upperLimit = coolingTypeLimits[batteryInfo.coolingType][1];
+
+	 return inferBreach(batteryInfo.temperature, lowerLimit, upperLimit);
+}
+
+BreachType inferBreach(double value, double lowerLimit, double upperLimit)
+{
+	bool stBuffer = STATUS_OK;
+	BreachType retVal = NORMAL;
+	if(value < lowerLimit)
+	{
+		stBuffer =  STATUS_NOT_OK;
+		retVal = TOO_LOW;
+	}
+	if(value > upperLimit)
+	{
+		stBuffer =  STATUS_NOT_OK;
+		retVal = TOO_HIGH;
+	}
+	BatteryStatus = stBuffer;
+	return retVal;
+}
