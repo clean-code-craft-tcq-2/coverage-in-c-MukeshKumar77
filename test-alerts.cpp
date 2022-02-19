e1c440544cc83cfa6f0781bcad82aa12560ed1dd
Mukesh <90908610+MukeshKumar77@users.noreply.github.com>
2022-02-19 18:06:10 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -1,8 +1,47 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "test/catch.hpp"
-#include "typewise-alert.h"
+#include "check_and_alert.h"
 
-TEST_CASE("infers the breach according to limits") {
-  REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
-}
+BatteryTypeAndInfo batteryData;
+
+	TEST_CASE("check email recipient set is correctly"){
+	setEmailRecepient("Kratos.GodOfWar@sony.com");
+	assert(!strcmp(EmailRecepient, "Kratos.GodOfWar@sony.com"));
+  }
+
+	TEST_CASE("test of check_and_alert method working") {
+	batteryData.coolingType = PASSIVE_COOLING;
+	batteryData.temperature = 24;
+	assert(checkAndAlert(TO_CONTROLLER, batteryData) == STATUS_OK);
+
+	batteryData.coolingType = PASSIVE_COOLING;
+	batteryData.temperature = 99;
+	assert(checkAndAlert(TO_EMAIL, batteryData) == STATUS_NOT_OK);
+  }
+
+	TEST_CASE("infers the breach according to limits") {
+	assert(inferBreach(12, 20, 30) == TOO_LOW);
+	assert(inferBreach(25, 20, 30) == NORMAL);
+	assert(inferBreach(31, 20, 30) == TOO_HIGH);
+  }
+
+	TEST_CASE("check working of cooling type classification") {
+	batteryData.coolingType = PASSIVE_COOLING;
+	batteryData.temperature = 36;
+	assert(classifyTemperatureBreach(batteryData) == TOO_HIGH);
+
+	batteryData.coolingType = HI_ACTIVE_COOLING;
+	batteryData.temperature = 46;
+	assert(classifyTemperatureBreach(batteryData) == TOO_HIGH);
+
+	batteryData.coolingType = MED_ACTIVE_COOLING;
+	batteryData.temperature = 41;
+	assert(classifyTemperatureBreach(batteryData) == TOO_HIGH);
+  }
+
+	TEST_CASE("check of sending message to either email or controller") {
+	assert(sendToController(TOO_HIGH) == true);
+	assert(sendToEmail(TOO_HIGH) == true);
+  }
+
